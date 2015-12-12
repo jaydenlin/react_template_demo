49c86bf2fe5ba664ebc5ada18c64263d88717841
weiyu.lin <weiyu.lin@rakuten.com>
2015-12-13 02:16:19 +0800

    remove useless codes

--- src/components/hello.jsx
+++ src/components/hello.jsx
@@ -3,16 +3,11 @@ var React = require('react/addons');
 var hellpTemplate = require("../templates/hello.rt");
 
 var Hello = React.createClass({
-  getInitialState: function() {
-    return {
-      name:"Jayden",
-      link:"jdlin.com"
-    };
-  },
+  
   render: function() {
-    //console.log(hellpTemplate);
+    
     return hellpTemplate();
-    //return <div>Hello World</div>
+    
   }
 
 });
