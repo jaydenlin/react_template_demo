ebddad3b0c40c46224373f45e809d87896586e4f
weiyu.lin <weiyu.lin@rakuten.com>
2015-12-13 02:31:52 +0800

    modify files for states

--- src/components/hello.jsx
+++ src/components/hello.jsx
@@ -3,10 +3,14 @@ var React = require('react/addons');
 var hellpTemplate = require("../templates/hello.rt");
 
 var Hello = React.createClass({
-  
+  getInitialState: function() {
+    return {
+      name:"Jayden" 
+    };
+  },
   render: function() {
     
-    return hellpTemplate();
+    return hellpTemplate.apply(this);
     
   }
 
