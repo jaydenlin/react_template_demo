4819c47651d4e1cd9c199bf5ea6469066b0cbf7c
weiyu.lin <weiyu.lin@rakuten.com>
2015-12-15 21:39:08 +0800

    change the render command

--- command/render.js
+++ command/render.js
@@ -1,10 +1,13 @@
 var path = require('path');
 var React = require('react/addons');
 require('node-jsx').install();
+require('../plugins/node-react-templates').install();
 
+//var compPath="../src/templates/hello.rt";
 var compPath="../src/components/hello.jsx";
 
 var component = require(compPath);
+//console.log(component());
 var contents = React.renderToString(React.createElement(component));
 
 console.log(contents);
